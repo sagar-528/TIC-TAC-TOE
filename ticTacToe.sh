2848c68788b7e4e6563cbe62443b2b4131ad94e4
sagar-528 <gupta.sagar528@gmail.com>
2020-03-23 21:24:01 +0530

    ADDED: Assigned sign

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -4,13 +4,25 @@
 #->Author: Sagar Gupta 
 #->Date: Mar 2020 
 
+#Constant
+X="X"
+O="O"
+
 declare -a board
 board=("1" "2" "3" "4" "5" "6" "7" "8" "9")
 echo "Welcome to Tic Tac Toe"
-
+echo " "
 #Setting Board
 echo "${board[0]} | ${board[1]} | ${board[2]}"
 echo "----------"
 echo "${board[3]} | ${board[4]} | ${board[5]}"
 echo "----------"
 echo "${board[6]} | ${board[7]} | ${board[8]}"
+
+echo " "
+case $((RANDOM%2)) in
+			0)
+			echo "You have assigned : " $O ;;
+			1)
+			echo "You have assigned : " $X ;;
+esac
