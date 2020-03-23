c9c0dcbaa421352de21a2aff23a6e462c18df5fc
sagar-528 <gupta.sagar528@gmail.com>
2020-03-24 00:21:25 +0530

    ADDED: Winning condition

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -59,6 +59,56 @@ else
 fi
 displayBoard
 }
+
+#Determining condition for winning or tie or change the turn
+function checkWin(){
+letter=$1
+
+if [ "${board[0]}${board[1]}${board[2]}" == "$letter$letter$letter" ]
+then
+		result="Win"
+elif [ "${board[3]}${board[4]}${board[5]}" == "$letter$letter$letter" ]
+then
+		result="Win"
+elif [ "${board[6]}${board[7]}${board[8]}" == "$letter$letter$letter" ]
+then
+		result="Win"
+elif [ "${board[0]}${board[3]}${board[6]}" == "$letter$letter$letter" ]
+then
+		result="Win"
+elif [ "${board[1]}${board[4]}${board[7]}" == "$letter$letter$letter" ]
+then
+		result="Win"
+elif [ "${board[2]}${board[5]}${board[8]}" == "$letter$letter$letter" ]
+then
+		result="Win"
+elif [ "${board[0]}${board[4]}${board[8]}" == "$letter$letter$letter" ]
+then
+		result="Win"
+elif [ "${board[2]}${board[4]}${board[6]}" == "$letter$letter$letter" ]
+then
+		result="Win"
+else
+		flag=0
+		for((counter=0; counter<${#board[@]}; counter++))
+		do
+			if [[ "${board[$choose]}"!=X  || "${board[$choose]}"!=O ]]
+			then
+					flag=1
+			fi
+		done
+
+	if [ $flag == 0 ]
+	then
+			result="draw"
+	else
+			result="change"
+	fi
+fi
+
+echo $result 
+}
 letter="$(getLetter)"
 player
 playerTurn $letter
+checkWin $playerSign
