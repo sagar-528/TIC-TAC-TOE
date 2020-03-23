00e811a28a6a3564def20d5384831241c56b59f8
sagar-528 <gupta.sagar528@gmail.com>
2020-03-23 23:34:18 +0530

    ADDED: My turns

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -9,30 +9,56 @@ X="X"
 O="O"
 
 declare -a board
-board=("1" "2" "3" "4" "5" "6" "7" "8" "9")
+board=("0" "1" "2" "3" "4" "5" "6" "7" "8")
 echo "Welcome to Tic Tac Toe"
 echo " "
 #Setting Board
+function displayBoard(){
 echo "${board[0]} | ${board[1]} | ${board[2]}"
 echo "----------"
 echo "${board[3]} | ${board[4]} | ${board[5]}"
 echo "----------"
 echo "${board[6]} | ${board[7]} | ${board[8]}"
+}
 
 #Assigning the letter in the game.
-echo " "
+function getLetter(){
 case $((RANDOM%2)) in
 			0)
-			echo "You have assigned : " $O ;;
+				playerSign=$X ;;
 			1)
-			echo "You have assigned : " $X ;;
+				playerSign=$O ;;
 esac
+echo $playerSign
+}
 
 #Who plays first in game
-echo " "
+function player(){
 case $((RANDOM%2)) in
 			0)
 			echo "You play first" ;;
 			1)
 			echo "Computer play first" ;;
 esac
+}
+
+#Choose the valid cells
+function playerTurn(){
+playerSign=$1
+read -p "Enter the cells between 0 to 8: " choose
+if [[ $choose>=0 && $choose<=8 ]]
+then
+	if [[ "${board[$choose]}"!=X || "${board[$choose]}"!=O ]]
+	then
+			board[$choose]="$playerSign"
+	else
+			playerTurn $playerSign
+	fi
+else
+	printf "Invalid input"
+fi
+displayBoard
+}
+letter="$(getLetter)"
+player
+playerTurn $letter
